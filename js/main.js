b3a355e864bc90b994812be3c52d143b13a14641
Shvana <huergojonatan4@gmail.com>
2023-06-11 09:33:34 +0800

    hero design

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const typed = new Typed(".auto-type", {
     strings: ["Jonatan Huergo Aguilar", "Software Engineering", "Graphic Designer", "Video Game Developer"],
-    typeSpeed : 45,
+    typeSpeed: 45,
     backSpeed: 50,
     loop: true
 });
